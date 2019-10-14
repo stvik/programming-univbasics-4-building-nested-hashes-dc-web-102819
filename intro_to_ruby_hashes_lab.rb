ad3ac3bf2b5a3a39e9a3d09d6ffad077e65d576c
Sara Tarnvik <stvik2@gmail.com>
2019-10-14 20:30:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,7 +43,7 @@ def monopoly_with_fourth_tier
     pieces: 4, 
     rent_in_dollars: {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200},
     names: 
-      {reading_railroad:{morgage_value:'$'100}, 
+      {reading_railroad:{morgage_value: "$100"}, 
       pennsylvania_railroad: {}, 
       b_and_o_railroad: {}, 
       shortline_railroad: {}
